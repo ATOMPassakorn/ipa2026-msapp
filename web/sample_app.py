ad918411ca65acb65cc23a2186b66beebc2cc06a
ATOMPassakorn <atom15440@gmail.com>
2026-09-06 22:14:13 +0000

    fix flake8 lint failure 7

--- web/sample_app.py
+++ web/sample_app.py
@@ -29,7 +29,11 @@ def add_router():
     password = request.form.get("password")
 
     if ip and username and password:
-        routers.insert_one({"ip": ip, "username": username, "password": password})
+        routers.insert_one(
+            {"ip": ip,
+            "username": username,
+            "password": password}
+        )
     return redirect("/")
 
 
